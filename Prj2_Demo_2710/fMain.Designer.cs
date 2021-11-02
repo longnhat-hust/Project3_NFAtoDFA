015487406e0fa0e7be4306784db3e9a24dcfc88d
Long Nhật Nguyễn <Long Nht Nguyn>
2021-11-03 01:58:23 +0700

    Add project files.

---
+++ Prj2_Demo_2710/fMain.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Project2_Recognition
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.txbPreview = new System.Windows.Forms.TextBox();
+            this.txbLink = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.txbWord = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnBrowser.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBrowser.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnBrowser.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnBrowser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBrowser.Font = new System.Drawing.Font("HelveticaNeue MediumCond", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowser.ForeColor = System.Drawing.Color.Black;
+            this.btnBrowser.Location = new System.Drawing.Point(3, 3);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(95, 29);
+            this.btnBrowser.TabIndex = 1;
+            this.btnBrowser.Text = "Browser";
+            this.btnBrowser.UseVisualStyleBackColor = false;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // txbPreview
+            // 
+            this.txbPreview.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbPreview.BackColor = System.Drawing.Color.Black;
+            this.txbPreview.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbPreview.ForeColor = System.Drawing.Color.Lime;
+            this.txbPreview.Location = new System.Drawing.Point(3, 38);
+            this.txbPreview.Multiline = true;
+            this.txbPreview.Name = "txbPreview";
+            this.txbPreview.ReadOnly = true;
+            this.txbPreview.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txbPreview.Size = new System.Drawing.Size(534, 444);
+            this.txbPreview.TabIndex = 4;
+            // 
+            // txbLink
+            // 
+            this.txbLink.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbLink.BackColor = System.Drawing.Color.Black;
+            this.txbLink.Font = new System.Drawing.Font("Linh Avenir", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbLink.ForeColor = System.Drawing.Color.White;
+            this.txbLink.Location = new System.Drawing.Point(104, 3);
+            this.txbLink.Name = "txbLink";
+            this.txbLink.Size = new System.Drawing.Size(433, 26);
+            this.txbLink.TabIndex = 0;
+            this.txbLink.TextChanged += new System.EventHandler(this.txbLink_TextChanged);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCheck.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCheck.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCheck.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnCheck.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCheck.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCheck.Font = new System.Drawing.Font("HelveticaNeue MediumCond", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.ForeColor = System.Drawing.Color.Black;
+            this.btnCheck.Location = new System.Drawing.Point(3, 439);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(603, 46);
+            this.btnCheck.TabIndex = 3;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = false;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // txbWord
+            // 
+            this.txbWord.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbWord.BackColor = System.Drawing.Color.Black;
+            this.txbWord.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbWord.ForeColor = System.Drawing.Color.Lime;
+            this.txbWord.Location = new System.Drawing.Point(3, 3);
+            this.txbWord.Multiline = true;
+            this.txbWord.Name = "txbWord";
+            this.txbWord.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txbWord.Size = new System.Drawing.Size(603, 430);
+            this.txbWord.TabIndex = 2;
+            this.txbWord.TextChanged += new System.EventHandler(this.txbWord_TextChanged);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txbLink);
+            this.splitContainer1.Panel1.Controls.Add(this.btnBrowser);
+            this.splitContainer1.Panel1.Controls.Add(this.txbPreview);
+            this.splitContainer1.Panel1MinSize = 95;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txbWord);
+            this.splitContainer1.Panel2.Controls.Add(this.btnCheck);
+            this.splitContainer1.Panel2MinSize = 92;
+            this.splitContainer1.Size = new System.Drawing.Size(1149, 485);
+            this.splitContainer1.SplitterDistance = 540;
+            this.splitContainer1.TabIndex = 7;
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1173, 509);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(393, 157);
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Project2_Recognition";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.TextBox txbPreview;
+        private System.Windows.Forms.TextBox txbLink;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox txbWord;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ Prj2_Demo_2710/fMain.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Project2_Recognition
+{
+    public partial class fMain : Form
+    {
+        Structure MainStructure;
+        State CurrentState;
+
+        public fMain()
+        {
+            InitializeComponent();
+        }
+        //
+        // Định nghĩa các Lớp
+        //
+        [XmlRoot(ElementName = "state")]
+        public class State
+        {
+            [XmlElement(ElementName = "x")]
+            public string X { get; set; }
+            [XmlElement(ElementName = "y")]
+            public string Y { get; set; }
+            [XmlElement(ElementName = "initial")]
+            public string Initial { get; set; }
+            [XmlAttribute(AttributeName = "id")]
+            public string Id { get; set; }
+            [XmlAttribute(AttributeName = "name")]
+            public string Name { get; set; }
+            [XmlElement(ElementName = "final")]
+            public string Final { get; set; }
+        }
+
+        [XmlRoot(ElementName = "transition")]
+        public class Transition
+        {
+            [XmlElement(ElementName = "from")]
+            public string From { get; set; }
+            [XmlElement(ElementName = "to")]
+            public string To { get; set; }
+            [XmlElement(ElementName = "read")]
+            public string Read { get; set; }
+        }
+
+        [XmlRoot(ElementName = "automaton")]
+        public class Automaton
+        {
+            [XmlElement(ElementName = "state")]
+            public List<State> ListState { get; set; }
+            [XmlElement(ElementName = "transition")]
+            public List<Transition> ListTransition { get; set; }
+        }
+
+        [XmlRoot(ElementName = "structure")]
+        public class Structure
+        {
+            [XmlElement(ElementName = "type")]
+            public string Type { get; set; }
+            [XmlElement(ElementName = "automaton")]
+            public Automaton Automaton { get; set; }
+        }
+        //
+        // HÀM Chuyển nội dung tệp XML thành các Lớp
+        //
+        Structure ConvertXMLtoClass(string LinkXML)
+        {
+            Structure struc = new Structure();
+            try
+            {              
+                XmlSerializer serializer = new XmlSerializer(typeof(Structure));
+                using (TextReader reader = new StringReader(File.ReadAllText(LinkXML)))
+                {
+                    struc = (Structure)serializer.Deserialize(reader);
+                }
+                return struc;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        //
+        // Kiểm tra tệp Jff có hợp lệ hay không
+        //
+        bool GetInfoFileJff(string link)
+        {
+            if (File.Exists(link))
+            {
+                FileInfo fi = new FileInfo(link);
+                if (fi.Exists && fi.Extension == ".jff")
+                {                 
+                    MainStructure = ConvertXMLtoClass(link); //Chuyển nội dung tệp XML thành Lớp MainStructure
+                    if (MainStructure != null)
+                    {
+                        string content = null;
+                        FileStream fs = new FileStream(link, FileMode.Open, FileAccess.Read);
+                        StreamReader sr = new StreamReader(fs);
+                        content = sr.ReadToEnd();
+                        sr.Close();
+                        fs.Close();
+
+                        txbLink.Text = link;
+                        txbPreview.Text = content;
+                        return true;
+                    }
+                    else return false;
+                }
+                else return false;
+            }
+            else return false;
+        }
+        //
+        // Chọn file
+        //
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "JFLAP files (*.jff)|*.jff";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                if (!GetInfoFileJff(ofd.FileName))
+                {
+                    MessageBox.Show("Error File!", "Oops...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        private void txbLink_TextChanged(object sender, EventArgs e)
+        {
+            GetInfoFileJff(txbLink.Text);
+        }
+        //
+        // 
+        //
+        State InitialState() //Hàm tìm Initial State
+        {
+            State inSt = new State();
+            foreach (State st in MainStructure.Automaton.ListState)
+            {
+                if (st.Initial != null)
+                {
+                    inSt = st;
+                    break;
+                }
+            }
+            return inSt;
+        }
+        State NextState(State curState, string chr) //Hàm trả về Next State với input là (Current State và kí tự tiếp theo)
+        {
+            State nextState = new State();          
+            foreach (Transition tran in MainStructure.Automaton.ListTransition)
+            {
+                if (curState.Id == tran.From && chr == tran.Read)
+                {
+                    foreach (State st in MainStructure.Automaton.ListState)
+                    {
+                        if (st.Id == tran.To)
+                        {
+                            nextState = st;
+                            return nextState;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+        //
+        // Hàm kiểm tra xâu vào có thuộc hay không
+        //
+        bool CheckWord(string word)
+        {
+            CurrentState = InitialState();
+            State tmpState;
+
+            char[] chrWord = word.ToCharArray();
+
+            foreach(char chr in chrWord)
+            {
+                tmpState = NextState(CurrentState, chr.ToString());
+                if (tmpState != null)
+                {
+                    CurrentState = tmpState;
+                }
+                else return false;
+            }
+
+            if (CurrentState.Final != null) //Kiểm tra trạng thái cuối cùng có phải Final không
+            {
+                return true;
+            }
+            else return false;
+        }
+        //
+        // Click button Check
+        //
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            if (CheckWord(txbWord.Text)) Accept();
+            else Reject();
+        }
+        //
+        //
+        //
+        void Accept()
+        {
+            txbWord.ForeColor = Color.Lime;
+            MessageBox.Show("Input is accepted","Result",MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        void Reject()
+        {
+            txbWord.ForeColor = Color.Red;
+            MessageBox.Show("Input is rejected", "Result", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private void txbWord_TextChanged(object sender, EventArgs e)
+        {
+            txbWord.ForeColor = Color.White;
+        }
+    }
+}
